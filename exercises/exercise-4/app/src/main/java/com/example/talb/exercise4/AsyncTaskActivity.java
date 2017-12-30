68dbbdac1f2bf78a7e51e600472424ef282c7e93
Tal Bar Zvi <tal.b@taboola.com>
2017-12-30 14:53:36 +0200

    Rename exercise-5

--- exercises/exercise-4/app/src/main/java/com/example/talb/exercise4/AsyncTaskActivity.java
+++ exercises/exercise-4/app/src/main/java/com/example/talb/exercise4/AsyncTaskActivity.java
@@ -2,6 +2,9 @@ package com.example.talb.exercise4;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +19,7 @@ public class AsyncTaskActivity extends AppCompatActivity implements UiUpdater {
     private Button createBtn;
     private int g;
     private int i;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,9 @@ public class AsyncTaskActivity extends AppCompatActivity implements UiUpdater {
         cancelBtn = findViewById(R.id.cancelBtn);
         startBtn = findViewById(R.id.startBtn);
         createBtn = findViewById(R.id.createBtn);
+        assert Looper.getMainLooper() == Looper.myLooper();
+        handler = new Handler(Looper.getMainLooper());
+
     }
 
     public void start(View view) {
@@ -39,6 +46,7 @@ public class AsyncTaskActivity extends AppCompatActivity implements UiUpdater {
     }
 
     public void create(View view) {
+        Message message = new Message();
         asyncTask = new AsyncTaskCounter(this);
         createBtn.setEnabled(false);
         startBtn.setEnabled(true);
@@ -52,11 +60,17 @@ public class AsyncTaskActivity extends AppCompatActivity implements UiUpdater {
     @Override
     public void resetButtons() {
         i = 0;
-        runOnUiThread(() -> {
+        handler.post(() -> {
             createBtn.setEnabled(true);
             startBtn.setEnabled(false);
             cancelBtn.setEnabled(false);
         });
+//        handler.sendMessage(ms)
+//        runOnUiThread(() -> {
+//            createBtn.setEnabled(true);
+//            startBtn.setEnabled(false);
+//            cancelBtn.setEnabled(false);
+//        });
     }
 
     @Override
---
+++ exercises/exercise-4/app/src/main/java/com/example/talb/exercise4/MyHandler.java
@@ -0,0 +1,15 @@
+package com.example.talb.exercise4;
+
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by tal.b on 26/12/2017.
+ */
+
+public class MyHandler extends Handler {
+    @Override
+    public void handleMessage(Message msg) {
+        super.handleMessage(msg);
+    }
+}
